8470c41b3ca0d867e7d5e6267715987fe5ac4230
Dylan_Hermosillo <hermosillodylan@gmail.com>
2025-10-29 14:03:58 -0400

    .

--- 01_Pull_Check_Trim_Decontam/config.sh
+++ 01_Pull_Check_Trim_Decontam/config.sh
@@ -1,30 +1,24 @@
-# --- System files ---
-.DS_Store
-Thumbs.db
+# Working Dir
+export WORKING_DIR=/share/ivirus/dhermos
 
-# --- Log files ---
-*.log
+# Logs Dir
+export LOGS_DIR=./logs/
 
-# --- Temporary files ---
-*.tmp
-*.swp
+# Files -- change this txt to a list of your SRR id's
+export XFILE=$WORKING_DIR/project1_ids.txt
 
-# --- Build / output directories ---
-/dist/
-/build/
-/out/
+# Reads Dir for SRA Toolkit output
+export READS_DIR=$WORKING_DIR/01_SRA_TOOLKIT/reads
 
-# --- Environment files ---
-.env
-.env.local
+# QC Outputs
+export FASTQ_DIR=$WORKING_DIR/02_FASTQC/
+export FASTQ_BEFORE=$FASTQ_DIR/before_trimming
+export FASTQ_AFTER=$FASTQ_DIR/after_trimming
 
-# --- Python cache ---
-__pycache__/
-*.pyc
+# Trimming
+export TRIM_DIR=$WORKING_DIR/03_TRIMMOMATIC/
+export TRIMMED_READ=$TRIM_DIR/trimmed_reads
+export U
 
-# --- Node modules ---
-node_modules/
-
-# --- IDE and editor files ---
-.vscode/
-.idea/
+# Contam Removal
+export CONTAM_DIR=$WORKING_DIR/04_Bowtie2/
