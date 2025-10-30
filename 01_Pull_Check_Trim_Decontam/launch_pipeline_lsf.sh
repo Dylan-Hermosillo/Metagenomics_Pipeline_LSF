0ab1f2981371e546aea78e1044118acf093f8bdf
Andarcher <Hermosillodylan@gmail.com>
2025-10-29 22:06:36 -0400

    job id fix in the launcher script

--- 01_Pull_Check_Trim_Decontam/launch_pipeline_lsf.sh
+++ 01_Pull_Check_Trim_Decontam/launch_pipeline_lsf.sh
@@ -42,67 +42,79 @@ create_dir $CONTAM_DIR $CONTAM_LOGS_O $CONTAM_LOGS_E
 # --- Launch Pipeline Steps ---
 # Job 1: SRA Prefetch
 echo "Launching Job 1: SRA Prefetch"
-bsub -J "$JOB1[1-$NUM_JOB]%$NUM_JOB" \
+JOBID1=$(bsub -J "$JOB1[1-$NUM_JOB]%$NUM_JOB" \
     -n $JOB1_CPUS \
     -q $JOB1_QUEUE \
     -R "rusage[mem=$JOB1_MEMORY]" \
     -W $JOB1_TIME \
     -o "${SRA_LOGS_O}/output.01A.%J_%I.log" \
     -e "${SRA_LOGS_E}/error.01A.%J_%I.log" \
-    < $RUN_SCRIPTS/${JOB1}.sh
+    < $RUN_SCRIPTS/${JOB1}.sh | awk '{print $2}' | tr -d '<>')
+echo "Submitted Job 1 array with ID $JOBID1"
+
 # Job 2: SRA Dump
 echo "Launching Job 2: SRA Dump"
-bsub -J "$JOB2[1-$NUM_JOB]%$NUM_JOB" \
+JOBID2=$(bsub -J "$JOB2[1-$NUM_JOB]%$NUM_JOB" \
     -n $JOB2_CPUS \
     -q $JOB2_QUEUE \
     -R "rusage[mem=$JOB2_MEMORY]" \
     -W $JOB2_TIME \
-    -w "done(${JOB1})" \
+    -w "done($JOBID1)" \
     -o "${SRA_LOGS_O}/output.01B.%J_%I.log" \
     -e "${SRA_LOGS_E}/error.01B.%J_%I.log" \
-    < $RUN_SCRIPTS/${JOB2}.sh
+    < $RUN_SCRIPTS/${JOB2}.sh | awk '{print $2}' | tr -d '<>')
+echo "Submitted Job 2 array with ID $JOBID2"
+
 # Job 3: FastQC Before Trim
 echo "Launching Job 3: FastQC Before Trim"
-bsub -J "$JOB3[1-$NUM_JOB]%$NUM_JOB" \
+JOBID3=$(bsub -J "$JOB3[1-$NUM_JOB]%$NUM_JOB" \
     -n $JOB3_CPUS \
     -q $JOB3_QUEUE \
     -R "rusage[mem=$JOB3_MEMORY]" \
     -W $JOB3_TIME \
-    -w "done(${JOB2})" \
+    -w "done($JOBID2)" \
     -o "${FASTQC_LOGS_O}/output.01A.%J_%I.log" \
     -e "${FASTQC_LOGS_E}/error.01A.%J_%I.log" \
-    < $RUN_SCRIPTS/${JOB3}.sh
+    < $RUN_SCRIPTS/${JOB3}.sh | awk '{print $2}' | tr -d '<>')
+echo "Submitted Job 3 array with ID $JOBID3"
+
 # Job 4: Trimmomatic
 echo "Launching Job 4: Trimmomatic"
-bsub -J "$JOB4[1-$NUM_JOB]%$NUM_JOB" \
+JOBID4=$(bsub -J "$JOB4[1-$NUM_JOB]%$NUM_JOB" \
     -n $JOB4_CPUS \
     -q $JOB4_QUEUE \
     -R "rusage[mem=$JOB4_MEMORY]" \
     -W $JOB4_TIME \
-    -w "done(${JOB3})" \
+    -w "done($JOBID3)" \
     -o "${TRIM_LOGS_O}/output.03.%J_%I.log" \
     -e "${TRIM_LOGS_E}/error.03.%J_%I.log" \
-    < $RUN_SCRIPTS/${JOB4}.sh
+    < $RUN_SCRIPTS/${JOB4}.sh | awk '{print $2}' | tr -d '<>')
+echo "Submitted Job 4 array with ID $JOBID4"
+
 # Job 5: Bowtie2 Decontamination
 echo "Launching Job 5: Bowtie2 Decontamination"
-bsub -J "$JOB5[1-$NUM_JOB]%$NUM_JOB" \
+JOBID5=$(bsub -J "$JOB5[1-$NUM_JOB]%$NUM_JOB" \
     -n $JOB5_CPUS \
     -q $JOB5_QUEUE \
     -R "rusage[mem=$JOB5_MEMORY]" \
     -W $JOB5_TIME \
-    -w "done(${JOB4})" \
+    -w "done($JOBID4)" \
     -o "${CONTAM_LOGS_O}/output.04.%J_%I.log" \
     -e "${CONTAM_LOGS_E}/error.04.%J_%I.log" \
-    < $RUN_SCRIPTS/${JOB5}.sh
+    < $RUN_SCRIPTS/${JOB5}.sh | awk '{print $2}' | tr -d '<>')
+echo "Submitted Job 5 array with ID $JOBID5"
+
 # Job 6: FastQC After Trim
 echo "Launching Job 6: FastQC After Trim"
-bsub -J "$JOB6[1-$NUM_JOB]%$NUM_JOB" \
+JOBID6=$(bsub -J "$JOB6[1-$NUM_JOB]%$NUM_JOB" \
     -n $JOB6_CPUS \
     -q $JOB6_QUEUE \
     -R "rusage[mem=$JOB6_MEMORY]" \
     -W $JOB6_TIME \
-    -w "done(${JOB4})" \
+    -w "done($JOBID4)" \
     -o "${FASTQC_AFTER_LOGS_O}/output.01A.%J_%I.log" \
     -e "${FASTQC_AFTER_LOGS_E}/error.01A.%J_%I.log" \
-    < $RUN_SCRIPTS/${JOB6}.sh
+    < $RUN_SCRIPTS/${JOB6}.sh | awk '{print $2}' | tr -d '<>')
+echo "Submitted Job 6 array with ID $JOBID6"
 # --- End Launch Pipeline Steps ---
+echo "All jobs submitted successfully!"
